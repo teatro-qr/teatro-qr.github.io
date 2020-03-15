da4a81ee2c95031cfeccfaaf831dc0a61d14e3d7
Laureano Clausi <laureano.clausi@gmail.com>
2020-03-15 00:40:40 -0300

    agrego service worker

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,13 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.0.0/workbox-sw.js');
 
 workbox.routing.registerRoute(
-    /\.(?:png|gif|jpg|jpeg|svg)$/,
+    /\.(?:png|gif|jpg|jpeg|svg|mp3)$/,
     new workbox.strategies.NetworkFirst({
         cacheName: 'images',
     })
 );
 
-workbox.routing.registerRoute(
-    new RegExp('/index.html|/js/|/css/'),
-    new workbox.strategies.NetworkFirst()
-);
+// workbox.routing.registerRoute(
+//     new RegExp('/index.html|/js/|/css/'),
+//     new workbox.strategies.NetworkFirst()
+// );
